390a4db301b56ac15edbc5a41e017e0d76b31b92
karenamir <121817894+karenamir@users.noreply.github.com>
2023-11-03 18:53:33 +0200

    Add files via upload

---
+++ untitled3.py
@@ -0,0 +1,54 @@
+def merge_sort(arr):
+    if len(arr) <= 1:
+        return arr
+
+    mid = len(arr) // 2
+    left_half = arr[:mid]
+    right_half = arr[mid:]
+
+    left_half = merge_sort(left_half)
+    right_half = merge_sort(right_half)
+
+    return merge(left_half, right_half)
+
+def merge(left, right):
+    result = []
+    i = j = 0
+
+    while i < len(left) and j < len(right):
+        if left[i] < right[j]:
+            result.append(left[i])
+            i += 1
+        else:
+            result.append(right[j])
+            j += 1
+
+    result.extend(left[i:])
+    result.extend(right[j:])
+    return result
+
+def binary_search(arr, target):
+    low, high = 0, len(arr) - 1
+
+    while low <= high:
+        mid = (low + high) // 2
+        if arr[mid] == target:
+            return True
+        elif arr[mid] < target:
+            low = mid + 1
+        else:
+            high = mid - 1
+
+    return False
+
+def find_pairs_with_sum(S, x):
+    S = merge_sort(S)
+    result = []
+
+    for a in S:  
+        target = x - a
+        if binary_search(S, target):
+            result.append((a, target))
+
+    return result
+
